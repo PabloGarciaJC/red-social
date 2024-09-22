35a2509ab9fa93003a62bab8579c9fd404eb8365
pablogarciajc <pablogarciiajc@gmail.com>
2024-09-22 03:00:49 +0200

    se crean publicaciones en tiempo real

--- public/js/custom/publications/app.js
+++ public/js/custom/publications/app.js
@@ -3,11 +3,11 @@ class InitAppPublicationClass {
   showComments() {
     $(document).on('click', '.btn__comments', (e) => {
       e.preventDefault();
-      const $currentTarget = $(e.currentTarget);
-      const $wrapperComments = $currentTarget.closest('.justify-content-end').find('.wrapper-comments');
-      // Asegúrate de que solo se maneje un `slideToggle` por vez
-      if ($wrapperComments.is(':animated')) return; // Evita múltiples animaciones
-      // Usa `slideToggle` para la animación de deslizamiento
+      let $currentTarget = $(e.currentTarget);
+      let $wrapperComments = $currentTarget.closest('.justify-content-end').find('.wrapper-comments');
+      // Evita múltiples animaciones
+      if ($wrapperComments.is(':animated')) return;
+      // `slideToggle` para la animación de deslizamiento
       $wrapperComments.slideToggle();
     });
   }
@@ -67,22 +67,17 @@ class InitAppPublicationClass {
     }
 
     function updateCommentCount(formElement) {
-
       let commentButton = formElement.closest('.wrapper-comments').prev('.btn__comments');
-
       if (commentButton.length === 0) {
         console.error('El botón de comentarios no se encuentra en el DOM.');
         return;
       }
-
       let buttonText = commentButton.text().trim();
       let match = buttonText.match(/\d+/);
-
       if (!match) {
         console.error('El texto del botón no contiene un número válido.');
         return;
       }
-
       let currentCount = parseInt(match[0]);
       let newCount = currentCount + 1;
       commentButton.text(`Comentarios (${newCount})`);
@@ -90,11 +85,11 @@ class InitAppPublicationClass {
   }
 
   emojis() {
-    // Guardamos la referencia a 'this' para usarla en el evento click
-    const self = this;
+    // Guarda la referencia a 'this' para usarla en el evento click
+    let self = this;
     // Mostrar y ocultar el selector de emojis de manera contextual para cada publicación
     $('.form__comments').each(function () {
-      const form = $(this);
+      let form = $(this);
       // Mostrar/ocultar emojis solo dentro del formulario actual
       form.find('#emojiToggle').on('click', function () {
         form.find('.emoji-picker').toggle();
@@ -138,12 +133,9 @@ class InitAppPublicationClass {
   }
 
   create() {
-
-    const self = this;
-
+    let self = this;
     $('.form-publication__create').on('submit', (e) => {
       e.preventDefault();
-
 
       let form = $(e.currentTarget);
       let formData = new FormData(form[0]);
@@ -157,140 +149,9 @@ class InitAppPublicationClass {
         processData: false,
         contentType: false,
         success: function (response) {
-
           // Limpia el formulario y cierra el modal
           $('.form-publication__create')[0].reset(); // Resetea los campos del formulario
           $('#exampleModal').removeClass('modal--active'); // Cierra el modal
-
-          // let publication = response.publication;
-          // let user = publication.user;
-          // let contenido = (publication.contenido ?? '').trim();
-
-          // let cardHtml = `
-          //                 <div class="col-12 mb-3">
-          //                     <div class="card info-card sales-card">
-          //                         <div class="filter">
-          //                             <a class="icon" href="#" data-bs-toggle="dropdown">
-          //                                 <i class="bi bi-three-dots"></i>
-          //                             </a>
-          //                             <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-          //                                 <li class="dropdown-header text-start">
-          //                                     <span>Opciones</span>
-          //                                 </li>
-          //                                 <li>
-          //                                     <a class="dropdown-item eliminar-publication" href="${baseUrl}publicationDelete/${publication.id}">
-          //                                       Eliminar
-          //                                     </a>
-          //                                 </li>  
-          //                             </ul>
-          //                         </div>
-          //                         <div class="card-body">
-          //                             <div class="d-flex align-items-center pt-3">
-          //                                 <div class="news">
-          //                                     <div class="post-item clearfix">
-          //                                         <img src="/fotoPerfil/${user.fotoPerfil}" 
-          //                                             alt="${user.alias}'s profile picture"
-          //                                             class="rounded-circle" width="50" height="50"/>
-          //                                         <a href="/usuario/${user.alias}?estado=confirmado&idNotificacion=0">
-          //                                             ${user.alias}
-          //                                         </a>
-          //                                     </div>
-          //                                 </div>
-          //                             </div>
-          //                             ${publication.imagen ? `
-          //                                 <img src="/publicationImagen/${publication.imagen}" 
-          //                                     alt="Publication Image" 
-          //                                     class="img-fluid rounded mb-3" 
-          //                                     style="max-width: 100%; height: auto;">
-          //                             ` : ''}
-          //                             ${contenido !== '' ? `<p class="pt-3">${contenido}</p>` : ''}
-          //                             <hr>
-          //                             <div class="row justify-content-end">
-          //                                 <div class="col col-lg-2">
-          //                                     <div class="like" id="btn-like${publication.id}" onclick="like(${publication.id})">
-          //                                         Like
-          //                                     </div>
-          //                                 </div>
-          //                                 <div class="col col-lg-2 btn__comments">Comentarios (0)</div>
-
-          //                                 <div id="${publication.id}" class="wrapper-comments" style="display: none;">
-
-          //                                     <form action="${baseUrl}comentarioSave" method="POST" enctype="multipart/form-data" class="form__comments">
-          //                                         <meta name="csrf-token" content="{{ csrf_token() }}">
-          //                                         <input type="hidden" name="id" value="${publication.id}">
-
-          //                                         <div class="input-group">
-
-          //                                             <div class="file-select">
-          //                                                 <input type="file" name="imagen" id="imagenPublicacion${publication.id}" aria-label="Archivo">
-          //                                             </div>
-
-          //                                             <!-- Botón para mostrar/ocultar el selector de emojis -->
-          //                                             <button type="button" id="emojiToggle" class="btn btn-secondary">😄 Emojis</button>
-          //                                               <input type="text" class="form-control comentario-input" placeholder="Escribe tu Comentario" name="comentario">
-          //                                               <button class="btn btn-primary" type="submit">Enviar
-          //                                             </button>
-
-          //                                         </div>
-
-          //                                         <div class="text-center form__collapse">contraer Formulario</div>
-
-          //                                         <!-- Cuadro de emojis visible inicialmente -->
-          //                                         <div class="emoji-picker" style="display: none; margin-top: 10px;">
-          //                                             <span class="chat-container__emoji">😊</span>
-          //                                             <span class="chat-container__emoji">😂</span>
-          //                                             <span class="chat-container__emoji">😍</span>
-          //                                             <span class="chat-container__emoji">😉</span>
-          //                                             <span class="chat-container__emoji">😭</span>
-          //                                             <span class="chat-container__emoji">😎</span>
-          //                                             <span class="chat-container__emoji">😡</span>
-          //                                             <span class="chat-container__emoji">🥺</span>
-          //                                             <span class="chat-container__emoji">😜</span>
-          //                                             <span class="chat-container__emoji">🤔</span>
-          //                                             <span class="chat-container__emoji">👍</span>
-          //                                             <span class="chat-container__emoji">🙏</span>
-          //                                             <span class="chat-container__emoji">❤️</span>
-          //                                             <span class="chat-container__emoji">🎉</span>
-          //                                             <span class="chat-container__emoji">🔥</span>
-          //                                             <span class="chat-container__emoji">🤯</span>
-          //                                             <span class="chat-container__emoji">🤩</span>
-          //                                             <span class="chat-container__emoji">😇</span>
-          //                                             <span class="chat-container__emoji">🥳</span>
-          //                                             <span class="chat-container__emoji">🤪</span>
-          //                                             <span class="chat-container__emoji">👀</span>
-          //                                             <span class="chat-container__emoji">😏</span>
-          //                                             <span class="chat-container__emoji">💀</span>
-          //                                             <span class="chat-container__emoji">👻</span>
-          //                                             <span class="chat-container__emoji">🤤</span>
-          //                                             <span class="chat-container__emoji">😴</span>
-          //                                             <span class="chat-container__emoji">👑</span>
-          //                                             <span class="chat-container__emoji">💩</span>
-          //                                             <span class="chat-container__emoji">🦄</span>
-          //                                             <span class="chat-container__emoji">🐶</span>
-          //                                         </div>
-          //                                     </form>
-          //                                 </div>
-          //                             </div>
-          //                         </div>
-          //                     </div>
-          //                 </div>`;
-
-          // $('#exampleModal').after(cardHtml);
-
-          // Llama a los métodos usando la referencia 'self'
-          // self.showComments();
-          // self.save();
-          // self.collapseComments();
-          // self.emojis();
-
-          // Cierra el modal y elimina el backdrop con un retraso
-          // $('#exampleModal').modal('hide');
-
-          // Asegúrate de eliminar el backdrop
-          // document.querySelectorAll('.modal-backdrop').forEach(function (backdrop) {
-          //   backdrop.remove(); // Elimina el backdrop del DOM
-          // });
-
           // Mostrar un mensaje de éxito usando SweetAlert
           Swal.fire({
             icon: 'success',
@@ -298,61 +159,61 @@ class InitAppPublicationClass {
             showConfirmButton: false,
             timer: 1000
           });
-
           $('#exampleModal').removeClass('modal--active').fadeOut();
         }
       });
     });
   }
 
- modal() {
-    const $imageFileInput = $('#imageFile');
-    const $imagePreview = $('#imagePreview');
-    const $commentTextarea = $('#commentTextarea');
+  modal() {
+    let $imageFileInput = $('#imageFile');
+    let $imagePreview = $('#imagePreview');
+    let $commentTextarea = $('#commentTextarea');
 
     $imageFileInput.on('change', function (event) {
-        const file = event.target.files[0];
-        if (file) {
-            const reader = new FileReader();
+      let file = event.target.files[0];
+      if (file) {
+        let reader = new FileReader();
 
-            reader.onload = function (e) {
-                $imagePreview.attr('src', e.target.result);
-                $imagePreview.show(); // Muestra la imagen
-            };
+        reader.onload = function (e) {
+          $imagePreview.attr('src', e.target.result);
+          // Muestra la imagen
+          $imagePreview.show();
+        };
 
-            reader.readAsDataURL(file);
-        } else {
-            $imagePreview.attr('src', '');
-            $imagePreview.hide(); // Oculta la imagen si no hay archivo
-        }
+        reader.readAsDataURL(file);
+      } else {
+        $imagePreview.attr('src', '');
+        // Oculta la imagen si no hay archivo
+        $imagePreview.hide();
+      }
     });
 
     $('#openModal').on('click', function () {
-        $('#exampleModal').addClass('modal--active').fadeIn();
+      $('#exampleModal').addClass('modal--active').fadeIn();
     });
 
     $('#closeModal, #closeModalFooter').on('click', function () {
-        $('#exampleModal').removeClass('modal--active').fadeOut();
+      $('#exampleModal').removeClass('modal--active').fadeOut();
     });
 
     $('#exampleModal').on('click', function (event) {
-        if (event.target === this) {
-            $('#exampleModal').removeClass('modal--active').fadeOut();
-        }
+      if (event.target === this) {
+        $('#exampleModal').removeClass('modal--active').fadeOut();
+      }
     });
 
     $('#emojiToggle').on('click', function () {
-        $('.modal__emoji-picker').toggle(); // Muestra u oculta el selector de emojis
+      $('.modal__emoji-picker').toggle(); // Muestra u oculta el selector de emojis
     });
 
     $('.modal__emoji-picker__emoji').on('click', function () {
-        const emoji = $(this).text();
-        const currentValue = $commentTextarea.val();
-        $commentTextarea.val(currentValue + emoji); // Inserta el emoji en el textarea
-        $('.modal__emoji-picker').hide(); // Opcional: Oculta el selector de emojis después de seleccionar
+      let emoji = $(this).text();
+      let currentValue = $commentTextarea.val();
+      $commentTextarea.val(currentValue + emoji); // Inserta el emoji en el textarea
+      $('.modal__emoji-picker').hide(); // Opcional: Oculta el selector de emojis después de seleccionar
     });
-}
-
+  }
 
   // Funcionalidades
   startIniPublication() {
@@ -364,8 +225,8 @@ class InitAppPublicationClass {
     this.create();
     this.modal();
   }
-
 }
 
 let initAppPublication = new InitAppPublicationClass();
-initAppPublication.startIniPublication();
+window.initAppPublication = initAppPublication;
+window.initAppPublication.startIniPublication();
--- resources/js/broadcast/publication.js
+++ resources/js/broadcast/publication.js
@@ -1,5 +1,125 @@
 window.Echo.channel('broadcastPublication-channel')
-.listen('.broadcastPublication-event', (e) => {
-    console.log(e);
-});
+    .listen('.broadcastPublication-event', (response) => {
+
+        let publication = response.publication;
+        let user = publication.user;
+        let contenido = (publication.contenido ?? '').trim();
+
+        let cardHtml = `
+                          <div class="col-12 mb-3">
+                              <div class="card info-card sales-card">
+                                  <div class="filter">
+                                      <a class="icon" href="#" data-bs-toggle="dropdown">
+                                          <i class="bi bi-three-dots"></i>
+                                      </a>
+                                      <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                                          <li class="dropdown-header text-start">
+                                              <span>Opciones</span>
+                                          </li>
+                                          <li>
+                                              <a class="dropdown-item eliminar-publication" href="${baseUrl}publicationDelete/${publication.id}">
+                                                Eliminar
+                                              </a>
+                                          </li>  
+                                      </ul>
+                                  </div>
+                                  <div class="card-body">
+                                      <div class="d-flex align-items-center pt-3">
+                                          <div class="news">
+                                              <div class="post-item clearfix">
+                                                  <img src="/fotoPerfil/${user.fotoPerfil}" 
+                                                      alt="${user.alias}'s profile picture"
+                                                      class="rounded-circle" width="50" height="50"/>
+                                                  <a href="/usuario/${user.alias}?estado=confirmado&idNotificacion=0">
+                                                      ${user.alias}
+                                                  </a>
+                                              </div>
+                                          </div>
+                                      </div>
+                                      ${publication.imagen ? `
+                                          <img src="/publicationImagen/${publication.imagen}" 
+                                              alt="Publication Image" 
+                                              class="img-fluid rounded mb-3" 
+                                              style="max-width: 100%; height: auto;">
+                                      ` : ''}
+                                      ${contenido !== '' ? `<p class="pt-3">${contenido}</p>` : ''}
+                                      <hr>
+                                      <div class="row justify-content-end">
+                                          <div class="col col-lg-2">
+                                              <div class="like" id="btn-like${publication.id}" onclick="like(${publication.id})">
+                                                  Like
+                                              </div>
+                                          </div>
+                                          <div class="col col-lg-2 btn__comments">Comentarios (0)</div>
+
+                                          <div id="${publication.id}" class="wrapper-comments" style="display: none;">
+
+                                              <form action="${baseUrl}comentarioSave" method="POST" enctype="multipart/form-data" class="form__comments">
+                                                  <meta name="csrf-token" content="{{ csrf_token() }}">
+                                                  <input type="hidden" name="id" value="${publication.id}">
+
+                                                  <div class="input-group">
+
+                                                      <div class="file-select">
+                                                          <input type="file" name="imagen" id="imagenPublicacion${publication.id}" aria-label="Archivo">
+                                                      </div>
+
+                                                      <!-- Botón para mostrar/ocultar el selector de emojis -->
+                                                      <button type="button" id="emojiToggle" class="btn btn-secondary">😄 Emojis</button>
+                                                        <input type="text" class="form-control comentario-input" placeholder="Escribe tu Comentario" name="comentario">
+                                                        <button class="btn btn-primary" type="submit">Enviar
+                                                      </button>
+
+                                                  </div>
+
+                                                  <div class="text-center form__collapse">contraer Formulario</div>
+
+                                                  <!-- Cuadro de emojis visible inicialmente -->
+                                                  <div class="emoji-picker" style="display: none; margin-top: 10px;">
+                                                      <span class="chat-container__emoji">😊</span>
+                                                      <span class="chat-container__emoji">😂</span>
+                                                      <span class="chat-container__emoji">😍</span>
+                                                      <span class="chat-container__emoji">😉</span>
+                                                      <span class="chat-container__emoji">😭</span>
+                                                      <span class="chat-container__emoji">😎</span>
+                                                      <span class="chat-container__emoji">😡</span>
+                                                      <span class="chat-container__emoji">🥺</span>
+                                                      <span class="chat-container__emoji">😜</span>
+                                                      <span class="chat-container__emoji">🤔</span>
+                                                      <span class="chat-container__emoji">👍</span>
+                                                      <span class="chat-container__emoji">🙏</span>
+                                                      <span class="chat-container__emoji">❤️</span>
+                                                      <span class="chat-container__emoji">🎉</span>
+                                                      <span class="chat-container__emoji">🔥</span>
+                                                      <span class="chat-container__emoji">🤯</span>
+                                                      <span class="chat-container__emoji">🤩</span>
+                                                      <span class="chat-container__emoji">😇</span>
+                                                      <span class="chat-container__emoji">🥳</span>
+                                                      <span class="chat-container__emoji">🤪</span>
+                                                      <span class="chat-container__emoji">👀</span>
+                                                      <span class="chat-container__emoji">😏</span>
+                                                      <span class="chat-container__emoji">💀</span>
+                                                      <span class="chat-container__emoji">👻</span>
+                                                      <span class="chat-container__emoji">🤤</span>
+                                                      <span class="chat-container__emoji">😴</span>
+                                                      <span class="chat-container__emoji">👑</span>
+                                                      <span class="chat-container__emoji">💩</span>
+                                                      <span class="chat-container__emoji">🦄</span>
+                                                      <span class="chat-container__emoji">🐶</span>
+                                                  </div>
+                                              </form>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>`;
+
+        $('#exampleModal').after(cardHtml);
+
+        // Llamada a los métodos desde la instancia
+        window.initAppPublication.showComments();
+        window.initAppPublication.save();
+        window.initAppPublication.collapseComments();
+        window.initAppPublication.emojis();
+    });
 
