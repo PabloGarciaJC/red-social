323525c281b93a7c3de4672e76fa7de6616bfbc3
pablogarciajc <pablogarciiajc@gmail.com>
2024-09-26 02:29:44 +0200

    se crea clase para archivo de config, variables globales

--- public/js/custom/config/config.js
+++ public/js/custom/config/config.js
@@ -1,3 +1,20 @@
-let url = new URL(window.location.href);
-let baseUrl = url.origin + '/';
-let userLogin = $('#user-data').data('user-id');
+class configClass {
+    constructor() {
+        this.url = "";
+        this.baseUrl = "";
+        this.userLogin = "";
+    }
+
+    setPageData() {
+        window.url = new URL(window.location.href);        
+        window.baseUrl = window.url.origin + '/';       
+        window.userLogin = $('#user-data').data('user-id');
+    }
+
+    init() {
+        this.setPageData();
+    }
+}
+
+let initConfig = new configClass();
+initConfig.init(); 
