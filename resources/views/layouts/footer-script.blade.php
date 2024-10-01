b9b582d5db7d22d100e8eece06df0e7c2f87eada
pablogarciajc <pablogarciiajc@gmail.com>
2024-10-01 03:01:24 +0200

    se corrige rutas de script en el footer

--- resources/views/layouts/footer-script.blade.php
+++ resources/views/layouts/footer-script.blade.php
@@ -3,11 +3,11 @@
         <script src="{{ asset('js/custom/main.js') }}"></script>
         <script src="{{ asset('js/custom/config/config.js') }}"></script>
         <script src="{{ asset('js/custom/user/user.js') }}"></script>
-        <script src="{{ asset('js/custom/comments/comments.js') }}"></script>
-        <script src="{{ asset('js/custom/publications/publications.js') }}"></script>
         <script src="{{ asset('js/custom/likes/like.js') }}"></script>
         <script src="{{ asset('js/custom/followers/followers.js') }}"></script>
         <script src="{{ asset('js/custom/chat/chat.js') }}"></script>
+        <script src="{{ asset('js/custom/comments/comments.js') }}"></script>
+        <script src="{{ asset('js/custom/publications/publications.js') }}"></script>
         <script src="{{ asset('js/custom/game/game.js') }}"></script>
         @stack('scripts')
     </body>
