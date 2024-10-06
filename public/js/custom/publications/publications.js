190a3bc4223bda9bd06e21181303fb663a8d3fa1
pablogarciajc <pablogarciiajc@gmail.com>
2024-10-06 13:48:53 +0200

    de descomenta publication

--- public/js/custom/publications/publications.js
+++ public/js/custom/publications/publications.js
@@ -72,20 +72,18 @@ class PublicationClass {
         processData: false,
         contentType: false,
         success: function (response) {
-
-          console.log(response);
-          // // Resetear el formulario después de éxito
-          // $('.form-publication__create')[0].reset();
-          // $('#exampleModal').removeClass('modal--active');
-          // // Limpiar las imágenes previsualizadas
-          // $('.modal__image-wrapper').empty();
-          // Swal.fire({
-          //   icon: 'success',
-          //   title: 'Publicación Creada',
-          //   showConfirmButton: false,
-          //   timer: 1000
-          // });
-          // $('#exampleModal').removeClass('modal--active').fadeOut();
+          // Resetear el formulario después de éxito
+          $('.form-publication__create')[0].reset();
+          $('#exampleModal').removeClass('modal--active');
+          // Limpiar las imágenes previsualizadas
+          $('.modal__image-wrapper').empty();
+          Swal.fire({
+            icon: 'success',
+            title: 'Publicación Creada',
+            showConfirmButton: false,
+            timer: 1000
+          });
+          $('#exampleModal').removeClass('modal--active').fadeOut();
         }
       });
 
