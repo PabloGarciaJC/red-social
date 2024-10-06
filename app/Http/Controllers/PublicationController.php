95f77b886f85a9737261d51fda5bcb71007018cb
pablogarciajc <pablogarciiajc@gmail.com>
2024-10-06 12:51:16 +0200

    codigo error, publicacion, TES

--- app/Http/Controllers/PublicationController.php
+++ app/Http/Controllers/PublicationController.php
@@ -46,20 +46,18 @@ class PublicationController extends Controller
                 // Nombre de la Imagen Original del Usuario y el Tiempo en que lo Sube
                 $imagenPathName = time() . '_' . $imagen->getClientOriginalName();
 
-                var_dump($imagenPathName);
+                // Guardo la Imagen en la carpeta del Proyecto
+                Storage::disk('publication')->put($imagenPathName, File::get($imagen));
 
-                // // Guardo la Imagen en la carpeta del Proyecto
-                // Storage::disk('publication')->put($imagenPathName, File::get($imagen));
+                // Guarda la ruta de la imagen en la tabla publication_images
+                DB::table('publication_images')->insert([
+                    'publication_id' => $publication->id,
+                    'image_path' => $imagenPathName,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
 
-                // // Guarda la ruta de la imagen en la tabla publication_images
-                // DB::table('publication_images')->insert([
-                //     'publication_id' => $publication->id,
-                //     'image_path' => $imagenPathName,
-                //     'created_at' => now(),
-                //     'updated_at' => now(),
-                // ]);
-
-                // array_push($imagePaths, $imagenPathName);
+                array_push($imagePaths, $imagenPathName);
             }
         }
 
