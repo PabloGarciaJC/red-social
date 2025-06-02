12cac161a9d06ba2362facb5e2a6b85425e116e8
PabloGarciaJC <pablogarciajc@hotmail.com>
2025-06-02 21:48:41 +0200

    se actualuza head

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
@@ -16,4 +16,29 @@
     @stack('styles')
 </head>
 
+<body> --}}
+
+<!DOCTYPE html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
+    <link href="{{ asset('images/logo.webp') }}" rel="icon">
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    @stack('styles')
+</head>
+
 <body>
+
+
+
+
+
