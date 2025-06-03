ced75d6d57865627d9a80753cfd29f7d11b30365
PabloGarciaJC <pablogarciajc@hotmail.com>
2025-06-03 09:20:27 +0200

    Forzando subir todos los cambios

---
+++ storage/framework/views/087709a097b5c2a498b7239cf64c9e53e11cce13.php
@@ -0,0 +1,3 @@
+  
+  <div id="user-data" data-user-id="<?php echo e(Auth::user()->id); ?>"></div>
+<?php /**PATH /var/www/html/resources/views/home/footer.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/4d0c7d162f0b256f1aa976fa4c290517adf2b04b.php
@@ -0,0 +1,23 @@
+  
+  <aside id="sidebar" class="sidebar">
+    <ul class="sidebar-nav" id="sidebar-nav">
+        <li class="nav-item">
+            <a class="nav-link " href="<?php echo e(route('home')); ?>">
+                <i class="bi bi-house"></i>
+                <span>Inicio</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link " href="<?php echo e(route('game.show')); ?>">
+                <i class="bi bi-grid"></i>
+                <span>Juegos</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link " href="<?php echo e(route('intro')); ?>">
+                <i class="bi bi-info-circle"></i>
+                <span>Conoce la Plataforma</span>
+            </a>
+        </li>
+    </ul>
+</aside><?php /**PATH /var/www/html/resources/views/home/sidebar.blade.php ENDPATH**/ ?>
